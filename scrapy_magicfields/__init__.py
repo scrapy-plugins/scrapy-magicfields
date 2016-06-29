a9e66eb213e717c6bd8d3d35a6f46b84267b9e74
Paul Tremberth <paul.tremberth@gmail.com>
2016-06-29 16:17:28 +0200

    Initial migration from scrapylib codebase

---
+++ scrapy_magicfields/__init__.py
@@ -0,0 +1,4 @@
+from .middleware import MagicFieldsMiddleware
+
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='scrapy-magicfields',
+    version='0.1.0',
+    license='BSD',
+    description='Scrapy middleware to add extra "magic" fields to items',
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    url='http://github.com/scrapy-plugins/scrapy-magicfields',
+    packages=['scrapy_magicfields'],
+    platforms=['Any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=['scrapy']
+)
