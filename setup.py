058154ceaf63f9687f8691ea389bf2f9f30bb0c7
Paul Tremberth <paul.tremberth@gmail.com>
2016-06-30 10:33:58 +0200

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapy-magicfields',
-    version='1.0.0',
+    version='1.1.0',
     license='BSD',
     description='Scrapy middleware to add extra "magic" fields to items',
     author='Scrapinghub',
